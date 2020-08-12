80dc95fc632058bbbab75c65f59acb26a95f2007
Ingus Jansons <ingusjancorp@gmail.com>
2020-08-12 18:00:34 +0100

    Stack and better buttons

--- src/App.js
+++ src/App.js
@@ -4,6 +4,25 @@ import "./index.css";
 import LinkBox from "./components/LinkBox";
 import ExpBox from "./components/ExpBox";
 import vf from "./img/vf.png";
+import illumi from "./img/illumi-dark.png";
+import csites from "./img/creatorsites.png";
+import react from "./img/react.svg";
+import express from "./img/express.png";
+import mongodb from "./img/mongodb.png";
+import nodejs from "./img/nodejs.svg";
+import php from "./img/php.png";
+import mysql from "./img/mysql.png";
+
+function getAge(dateString) {
+  var today = new Date();
+  var birthDate = new Date(dateString);
+  var age = today.getFullYear() - birthDate.getFullYear();
+  var m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+  return age;
+}
 
 function App() {
   return (
@@ -13,13 +32,14 @@ function App() {
           src={"./me-min.jpg"}
           style={{
             width: "160px",
-            borderRadius: "35% 65% 70% 30% / 52% 47% 53% 48% ",
+            borderRadius: "50%",
             marginBottom: "15px",
           }}
         />
+
         <h1 className="title is-2">Ingus Jansons</h1>
-        <h1 className="subtitle is-5 is-short" style={{ marginTop: "10px" }}>
-          Working at Vodafone Group{" "}
+        <h2 className="subtitle is-5 is-short" style={{ marginTop: "10px" }}>
+          {getAge("2001/06/13")}, Working at Vodafone Group{" "}
           <img
             src={vf}
             width={20}
@@ -30,7 +50,7 @@ function App() {
           />{" "}
           as a Software Engineer while also studying for my degree (B.Sc.) -
           Living in Newbury 🇬🇧
-        </h1>
+        </h2>
 
         <ul style={{ marginBottom: "20px" }}>
           <li>- Natively Latvian (born in Riga, LV) 🇱🇻</li>
@@ -68,23 +88,64 @@ function App() {
           className="title is-4"
           style={{ marginTop: "45px", marginBottom: "25px" }}
         >
+          Stack
+        </h2>
+
+        <div className="buttons">
+          <img src={react} style={{ height: "64px" }}></img>
+          <img
+            src={mongodb}
+            style={{ height: "55px", marginRight: "20px" }}
+          ></img>
+          <img
+            src={express}
+            style={{ height: "54px", marginRight: "20px" }}
+          ></img>
+          <img
+            src={nodejs}
+            style={{ height: "54px", marginRight: "20px" }}
+          ></img>
+          <img src={php} style={{ height: "62px", marginRight: "20px" }}></img>
+          <img
+            src={mysql}
+            style={{ height: "50px", marginRight: "20px" }}
+          ></img>
+        </div>
+
+        <p style={{ marginTop: "25px" }}>
+          My technology stack changes as the waves in web development change,
+          but being a fan of companies like Stripe and Uber for their design,{" "}
+          <br />
+          my stack will remain in the world of single page applications and REST
+          APIs.
+        </p>
+        <h2
+          className="title is-4"
+          style={{ marginTop: "45px", marginBottom: "25px" }}
+        >
           Projects
         </h2>
 
         <div className="columns">
           <div className="column">
             <LinkBox
+              imgStyles={{ width: "110px" }}
+              img={illumi}
               link="https://illumimedia.co/"
               title="Illumi Media"
-              desc="Design powerhouse for content creators"
+              desc="Design powerhouse for creators"
+              story="It started off at the age of 15 when I helped my friends with some graphic design work and quickly, through some inner friendship circles gained access to huge talent that needed branding. From there on, I built the Illumi Media brand."
             />
           </div>
 
           <div className="column">
             <LinkBox
+              imgStyles={{ width: "60px" }}
+              img={csites}
               link="https://creatorsites.net"
               title="Creator Sites"
-              desc="Store all of your links in one place."
+              desc="All your links in one place"
+              story="Being my first ever production project, Creator Sites was born because I was frustrated with how little link aggregators offered, while being the main entry point for most content creators. I now have over 70 active users, many of whom are large content creators."
             />
           </div>
 
@@ -92,10 +153,12 @@ function App() {
             <LinkBox
               link="https://spacex-tracker.netlify.app"
               title="SpaceX Tracker"
-              desc="(Almost) accurate data about SpaceX launches."
+              desc="(Almost) accurate data about SpaceX launches"
+              story="There's not much of a story to this one. I found a SpaceX API and wanted to play around with it and create a minimalistic, yet useful tracker for all flights and even a full-screen countdown. I did just that, designed and deployed through Netlify within 3 days!"
             />
           </div>
         </div>
+
         <h2 className="title is-4" style={{ marginTop: "15px" }}>
           Experience
         </h2>
--- src/components/LinkBox.js
+++ src/components/LinkBox.js
@@ -6,10 +6,18 @@ export default class LinkBox extends Component {
       <div>
         <a href={this.props.link} target="_blank" type="external">
           <div className="box link-box">
-            <h3 className="subtitle is-5" style={{ marginBottom: "5px" }}>
-              {this.props.title}
-            </h3>
-            <p>{this.props.desc}</p>
+            <div className="columns">
+              <div className="column is-narrow" style={{ marginRight: "20px" }}>
+                <img src={this.props.img} style={this.props.imgStyles}></img>
+              </div>
+              <div className="column">
+                <h3 className="subtitle is-5" style={{ marginBottom: "5px" }}>
+                  {this.props.title}
+                </h3>
+                <p style={{ color: "#c15000" }}>{this.props.desc}</p>
+              </div>
+            </div>
+            {this.props.story && <p className="help">{this.props.story}</p>}
           </div>
         </a>
       </div>
