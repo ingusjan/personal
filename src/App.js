2018bbce6f9a1324ec11f71e87ae1ec1b27581a8
Ingus <ingusjan@gmail.com>
2020-12-26 19:29:32 +0000

    Added Projects

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <div className={`${dark && "dark"} `}>
-      <div className="w-screen h-screen leading-relaxed overflow-y-scroll p-6 xl:px-48 xl:py-16 bg-gray-100 dark:bg-gray-900 dark:text-gray-200">
+      <div className="w-screen h-screen leading-normal overflow-y-scroll p-6 xl:px-48 xl:py-16 bg-gray-100 dark:bg-gray-900 dark:text-gray-200">
         <div
           className="h-64 w-full rounded-2xl"
           style={{
@@ -99,24 +99,62 @@ function App() {
 
           <h3 className="text-xl mt-12 font-semibold">Projects</h3>
 
-          <div className="mt-5">
+          <div className="mt-5 2xl:flex content-center items-center">
             <Project
               name="Creator Sites"
-              desc="Simple link sharing for Content Creators"
+              type="Fullstack Web Application"
+              img="https://pbs.twimg.com/profile_images/1283419053730738177/Cyd5invl.jpg"
               link="https://creatorsites.net"
+              desc="Developed API & Frontend for a link aggregator platform. It allows Content Creators to have one single link to house all of their social media. Used by over 180+ creators."
+              tech={["MongoDB", "React", "Express", "Heroku"]}
               status="Active"
             />
+
             <Project
               name="Illumi Media"
-              desc="Premium Design for Content Creators"
+              type="Premium Design for Content Creators"
+              img="https://pbs.twimg.com/profile_images/1262699046868828160/nRqdCD9W_400x400.png"
+              desc="Putting the Graphic Design skills to use, Illumi Media now scores over 200+ 5-star reviews on Fiverr and over 300+ orders in total."
+              tech={["Photoshop", "Fiverr", "WooCommerce", "WordPress"]}
               link="https://illumimedia.co"
-              status="Active"
             />
+
             <Project
               name="Webarc Hosting"
-              desc="Affordable UK-based web Hosting"
+              type="Affordable UK-based Web Hosting"
+              img="https://s3-eu-west-1.amazonaws.com/tpd/logos/5f9944892ac35b0001df46b0/0x0.png"
+              desc="In late 2020 I founded Webarc Hosting which allowed customers to register domain names and order Shared Hosting for Wordpress and other platforms."
+              tech={["WHMCS", "WHM", "cPanel"]}
               link="https://webarc.host"
-              status="Inactive"
+            />
+          </div>
+
+          <div className="mt-2 2xl:flex content-center items-center">
+            <Project
+              name="Bass Arena UK"
+              type="EDM Internet Radio"
+              img="https://scontent-lhr8-2.xx.fbcdn.net/v/t31.0-8/13122959_221820811526743_5338576870745899407_o.png?_nc_cat=105&ccb=2&_nc_sid=09cbfe&_nc_ohc=x5f5y5Uc0xAAX9Xas-Q&_nc_ht=scontent-lhr8-2.xx&oh=789ee0c34dd2d471cd7df9d65085dee5&oe=600E6562"
+              link="https://facebook.com/bassarenauk/"
+              desc="Developed the website, brand and Shoutcast server for a now-defunct EDM Internet Radio during 2016. A lot of the music played was copyright-free and licensed."
+              tech={["Shoutcast", "Radio-over-IP", "RadioDJ"]}
+            />
+
+            <Project
+              name="Almighty Records"
+              type="eCommerce Redesign"
+              img="https://img.endole.co.uk/?t=273e37ce59721093a775b44c15e6737e&l="
+              desc="With the company’s chosen tech stack being Squarespace, I had the responsibility of designing a refreshed, modern user experience for the British Heritage record label."
+              tech={["Squarespace", "Adobe Photoshop"]}
+              link="https://almightyrecords.com"
+            />
+
+            <Project
+              name="SpaceX Tracker"
+              type="Utilizing SpaceX's Open-API"
+              img="https://res-5.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco/u196odmivjn2dvw09xhx"
+              desc="Developed a very basic, yet useful tracker for SpaceX's launches. It shows a history of launches and a full-screen countdown to the next one."
+              tech={["React", "Axios", "Netlify"]}
+              link="https://spacex.ingus.dev"
             />
           </div>
 
--- src/components/Project.js
+++ src/components/Project.js
@@ -2,29 +2,40 @@ import React from "react";
 
 export default function Project(props) {
   return (
-    <div className="py-3 px-4 mb-3 rounded-sm shadow-lg bg-gray-50 dark:bg-gray-800 lg:w-1/2">
-      <h4 className="text-lg">{props.name}</h4>
-      <p className="text-gray-400">{props.desc}</p>
+    <div className="py-4 px-5 mb-3 xl:w-1/3 min-w-1/3 max-w-1/3 h-full min-h-full max-h-full rounded-sm shadow-lg bg-gray-50 dark:bg-gray-800 lg:mr-5">
+      <div className="flex content-center items-center mb-5">
+        <div className="mr-5">
+          <img src={props.img} alt={props.name} className="rounded-full w-14" />
+        </div>
+        <div>
+          <h4 className="text-lg">{props.name}</h4>
+          <p className="text-primary-400">{props.type}</p>
+        </div>
+      </div>
+      <p>{props.desc}</p>
       <a
         href={props.link}
-        className="text-primary-400"
+        className="text-gray-400 underline"
         target="_blank"
         rel="noreferrer"
       >
         {props.link}
       </a>
-      <p className="text-sm text-gray-500 mt-2">
-        Status:{" "}
-        <span
-          className={`${
-            props.status === "Active"
-              ? "dark:text-green-500 text-green-600"
-              : "dark:text-red-500 text-red-600"
-          } font-medium`}
-        >
-          {props.status}
-        </span>
-      </p>
+
+      {props.tech && (
+        <div className="mt-3 mb-3">
+          <p className="text-primary-400 mb-2">Technology used</p>
+          <div className="overflow-x-auto flex content-center items-center w-full h-full">
+            {props.tech.map((x) => {
+              return (
+                <span className="dark:bg-gray-700 py-2 px-4 mr-2 text-sm rounded-full">
+                  {x}
+                </span>
+              );
+            })}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
