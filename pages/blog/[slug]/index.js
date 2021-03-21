a2c0dac5d3c35022cd72414f20ca2d184d52be09
Ingus <ingusjan@gmail.com>
2021-03-21 18:21:54 +0000

    feat: update contentful + fix <Bold> tag

--- pages/blog/[slug]/index.js
+++ pages/blog/[slug]/index.js
@@ -1,29 +1,46 @@
-import { useRouter } from "next/router";
 import { fetchEntries, fetchEntry } from "../../../util/contentfulPosts";
 import { BLOCKS, MARKS } from "@contentful/rich-text-types";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Link from "next/link";
+import Image from "next/image";
 
 const blog = ({ posts }) => {
   const options = {
     renderMark: {
-      [MARKS.BOLD]: (text) => <Bold>{text}</Bold>,
+      [MARKS.BOLD]: (text) => <p className="font-bold">{text}</p>,
     },
     renderNode: {
       [BLOCKS.EMBEDDED_ASSET]: (node, children) => (
         <img
-          className="lg:w-1/2 mt-5 rounded-md"
-          src={node.data.target.fields.file.url}
+          className="lg:w-1/3 mt-5 rounded-md"
+          quality="25%"
+          layout="responsive"
+          height="auto"
+          width="auto"
+          src={"https://" + node.data.target.fields.file.url}
         />
       ),
       [BLOCKS.UL_LIST]: (node, children) => (
-        <ul className="list-disc my-5">{children}</ul>
+        <ul className="my-5 list-disc">{children}</ul>
+      ),
+      [BLOCKS.LIST_ITEM]: (node, children) => (
+        <li className="list-inside list-disc">{children}</li>
       ),
       [BLOCKS.HEADING_2]: (node, children) => (
-        <h2 className="text-lg font-semibold my-5">{children}</h2>
+        <h2 className="text-xl font-semibold my-5">{children}</h2>
       ),
+      [BLOCKS.LIST_ITEM]: (node, children) => {
+        const UnTaggedChildren = documentToReactComponents(node, {
+          renderNode: {
+            [BLOCKS.PARAGRAPH]: (node, children) => children,
+            [BLOCKS.LIST_ITEM]: (node, children) => children,
+          },
+        });
+
+        return <li className="list-inside">{UnTaggedChildren}</li>;
+      },
     },
     renderText: (text) => text.replace("!", "?"),
   };
@@ -31,10 +48,10 @@ const blog = ({ posts }) => {
   dayjs.extend(relativeTime);
   return (
     <div>
-      <p className="mb-7 hover:underline">
+      <p className="mb-7 opacity-40 hover:underline">
         <Link href="/">Return to home</Link>
       </p>
-      <h1 className="font-bold text-2xl tracking-tight">{post.title}</h1>
+      <h1 className="font-bold text-3xl tracking-tight">{post.title}</h1>
       <p className="opacity-50">
         Published {dayjs(post.publishedDate).format("DD/MM/YYYY")}
         <span> • {dayjs(post.publishedDate).fromNow()}</span>
@@ -69,7 +86,7 @@ export const getStaticPaths = async () => {
   const paths = slugs.map((slug) => ({ params: { slug: slug } }));
   return {
     paths,
-    fallback: false
+    fallback: false,
   };
 };
 
