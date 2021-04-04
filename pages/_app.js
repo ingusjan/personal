4879aaec31d1cdc39f6af253da156972b321e7f0
Ingus Jansons <ingusjan@gmail.com>
2021-04-04 17:21:58 +0100

    feat: nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -2,24 +2,30 @@ import "tailwindcss/tailwind.css";
 import Layout from "../components/Layout";
 import Head from "next/head";
 import { ThemeProvider } from "next-themes";
+import NextNprogress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider enableSystem={false} defaultTheme="dark" attribute="class">
+    <ThemeProvider enableSystem={false} defaultTheme='dark' attribute='class'>
       <Layout>
         <Head>
           <meta
-            name="description"
-            content="Ingus Jansons is a Software Engineer based in Gloucester, UK - born in Riga, Latvia."
+            name='description'
+            content='Ingus Jansons is a Software Engineer based in Gloucester, UK - born in Riga, Latvia.'
           />
           <title>Ingus Jansons - Software Engineer</title>
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel='preconnect' href='https://fonts.gstatic.com' />
           <link
-            href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;500;600;700&display=swap"
-            rel="stylesheet"
+            href='https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;500;600;700&display=swap'
+            rel='stylesheet'
           />
         </Head>
-
+        <NextNprogress
+          color='#29D'
+          startPosition={0.3}
+          stopDelayMs={200}
+          height='3'
+        />
         <Component {...pageProps} />
       </Layout>
     </ThemeProvider>
