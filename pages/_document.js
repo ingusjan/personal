a04ffb2cedbd824158e39dea360b142989e4da04
Ingus <ingusjan@gmail.com>
2021-04-12 13:35:56 +0100

    feat: fonts load locally

--- pages/_document.js
+++ pages/_document.js
@@ -12,13 +12,7 @@ class MyDocument extends Document {
         <Head
           title="Ingus Jansons - Software Engineer"
           desc="Ingus Jansons is a Software Engineer in the UK born in Latvia"
-        >
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;500;600;700&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
+        ></Head>
         <body className="bg-gray-100 dark:bg-black dark:text-gray-100">
           <Main />
           <NextScript />
