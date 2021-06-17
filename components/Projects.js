684956f3a219f034daa64d729be7523f14630083
Ingus <ingusjan@gmail.com>
2021-06-17 17:55:44 +0100

    fix: profile pic in blog post

--- components/Projects.js
+++ components/Projects.js
@@ -42,9 +42,9 @@ const Projects = () => {
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 lg:gap-5 items-end">
-      {projects.map((p) => {
+      {projects.map((p, i) => {
         return (
-          <a href={p.link} target="_blank" rel="noopener">
+          <a key={i} href={p.link} target="_blank" rel="noopener">
             <div className="flex flex-col justify-center items-center p-4 bg-[#20242b] text-center h-64 rounded-lg hover:bg-[#393e46] border border-[#393e46] cursor-pointer transition">
               <img className="w-16 mb-2" src={p.img} alt={p.name} />
               <h3 className="text-lg font-semibold my-2">{p.name}</h3>
